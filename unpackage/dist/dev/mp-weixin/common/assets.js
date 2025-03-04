afc83d03cc42a0a803fcc6996ab317ace1c2b286
dinosaur2004 <2023302181251@whu.edu.cn>
2025-03-04 22:00:19 +0800

    del unpackage

--- unpackage/dist/dev/mp-weixin/common/assets.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-const _imports_0$1 = "/static/dinoundertreezoom.jpg";
-const _imports_0 = "/static/add.png";
-exports._imports_0 = _imports_0$1;
-exports._imports_0$1 = _imports_0;
-//# sourceMappingURL=../../.sourcemap/mp-weixin/common/assets.js.map
