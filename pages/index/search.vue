b2324289a41812d803e1131325f298976e297a0c
dinosaur2004 <2023302181251@whu.edu.cn>
2025-04-07 16:19:35 +0800

    fix filter

--- pages/index/search.vue
+++ pages/index/search.vue
@@ -55,7 +55,7 @@
       title: '星禾羽毛球运动中心',
       price: '50',
       address: '星禾羽毛球运动中心',
-      date: '02月07日 周五',
+      date: '04月13日 周五',
       time: '19:00-22:00',
       status: '正在报名',
       participants: 99,
@@ -67,7 +67,7 @@
       title: '幸福林带冠深酷动力羽毛球馆',
       price: '60',
       address: '幸福林带冠深酷动力羽毛球馆',
-      date: '02月07日 周五',
+      date: '04月13日 周五',
       time: '19:00-22:00',
       status: '报名结束',
       participants: 8,
@@ -79,7 +79,7 @@
       title: '西安奥体中心羽毛球馆',
       price: '50',
       address: '西安市国际港务区港丰路69号',
-      date: '02月07日 周五',
+      date: '04月13日 周五',
       time: '09:00-22:00',
       status: '正在报名',
       participants: 50,
@@ -91,7 +91,7 @@
       title: '新曈羽毛球训练中心',
       price: '60',
       address: '西安省体育馆BE中心',
-      date: '02月07日 周五',
+      date: '04月13日 周五',
       time: '10:00-22:00',
       status: '报名中',
       participants: 20,
@@ -103,7 +103,7 @@
       title: '西安西北大学羽毛球馆',
       price: '10',
       address: '西安市太白北路229号西北大学校内',
-      date: '02月07日 周五',
+      date: '04月13日 周五',
       time: '14:30-22:00',
       status: '报名中',
       participants: 30,
@@ -115,7 +115,7 @@
       title: '朱雀羽毛球馆',
       price: '35',
       address: '西安市碑林区长安路北段14号',
-      date: '02月07日 周五',
+      date: '04月13日 周五',
       time: '09:00-22:00',
       status: '正在报名',
       participants: 40,
@@ -127,7 +127,7 @@
       title: '西安博蓝羽毛球馆',
       price: '免费',
       address: '西安市经济开发区凤城十路99号蓝天院内',
-      date: '02月07日 周五',
+      date: '04月13日 周五',
       time: '08:00-22:00',
       status: '报名中',
       participants: 10,
@@ -449,7 +449,11 @@
   ];
   const showactivities = computed(()=>{
     let reg = new RegExp(searchinput.value) //声明正则变量
-    return activities.filter(item => reg.test(item.title)) //返回
+    let t=activities.filter(item => reg.test(item.title)) //返回
+    if (selectedFilter.value != 0 ){
+      t = t.filter(item => item.tag === filters[selectedFilter.value])
+    }
+    return t
   })
 </script>
 
